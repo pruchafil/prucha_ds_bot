c8228e67fb9258abc07c1a184ce71d0222bcfb2b
prucha.fil <prucha.fil@gmail.com>
2025-10-23 23:22:07 +0200

    fix error logging

--- user_actions.py
+++ user_actions.py
@@ -8,5 +8,5 @@ async def timeout_user(logger: Logger, user: discord.User | discord.Member, unti
         await user.timeout(until, reason=reason)
         logger.info(f"Successful timeout %s for %i minutes", user, until.min)
     except discord.Forbidden as e:
-        logger.error("failed to blocked user %s %s", user, e)
+        logger.error("failed to blocked user %s %s", user, e.text)
 
