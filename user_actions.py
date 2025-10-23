14635e97a2ad2873d2c958734abc4b91e22c5f99
prucha.fil <prucha.fil@gmail.com>
2025-10-23 21:22:12 +0200

    requirments.tst update, write exception

--- user_actions.py
+++ user_actions.py
@@ -1,4 +1,4 @@
-from logging import Logger
+from logging import Logger, exception
 
 import discord
 import datetime
@@ -7,6 +7,6 @@ async def timeout_user(logger: Logger, user: discord.User | discord.Member, unti
     try:
         await user.timeout(until, reason=reason)
         logger.info(f"Successful timeout %s for %i minutes", user, until.min)
-    except discord.Forbidden:
-        logger.error("failed to blocked user %s", user)
+    except discord.Forbidden as e:
+        logger.error("failed to blocked user %s %s", user, e)
 
